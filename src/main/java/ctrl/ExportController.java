80de587ae1a0b8c6a1204aa9004d52f56a068594
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-06-20 16:06:05 -0300

    Dependencia JUnit

---
+++ src/main/java/ctrl/ExportController.java
@@ -0,0 +1,7 @@
+package ctrl;
+
+/**
+ * Created by marcosdias on 20/06/15.
+ */
+public class ExportController {
+}
---
+++ src/test/java/test/ExportControllerTest.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by marcosdias on 20/06/15.
+ */
+public class ExportControllerTest {
+}
