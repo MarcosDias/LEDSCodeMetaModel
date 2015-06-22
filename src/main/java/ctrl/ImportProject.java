69c4ce0617d747708be307580bc34a771635b621
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-06-22 18:21:59 -0300

    Importacao de arquivos yaml para Project

--- src/main/java/ctrl/ImportProject.java
+++ src/main/java/ctrl/ImportProject.java
@@ -1,7 +1,23 @@
 package ctrl;
 
+import model.mainLayer.Project;
+import org.ho.yaml.Yaml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
- * Created by marcosdias on 22/06/15.
+ * @author MarcosDias
  */
 public class ImportProject {
+    /**
+     * Dado um arquivo com a representacao textual de um Projeto de Software,
+     * ele sera convertido novamente para um objeto do tipo Project.
+     * @param path Caminho do arquivo que sera importado
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static Project importFileYaml(String path) throws FileNotFoundException {
+        return Yaml.loadType(new File(path), Project.class);
+    }
 }
--- src/test/java/test/ImportProjectTest.java
+++
@@ -1,7 +0,0 @@
-package test;
-
-/**
- * Created by marcosdias on 22/06/15.
- */
-public class ImportProjectTest {
-}
