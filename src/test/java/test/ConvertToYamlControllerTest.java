d83d7527bd79fd8ae34cd55c9659c2df65f35b9d
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-06-22 17:23:58 -0300

    Unit Test - ConvertToYamlController - finalizado

--- src/test/java/test/ConvertToYamlControllerTest.java
+++ src/test/java/test/ConvertToYamlControllerTest.java
@@ -1,8 +1,27 @@
 package test;
 
-/**
- * Created by marcosdias on 20/06/15.
- */
-public class ConvertToYamlControllerTest {
+import ctrl.ConvertToYamlController;
+import junit.framework.TestCase;
+import model.mainLayer.Project;
 
+import static ctrl.ConvertToYamlController.convert;
+import static test.util.SimpleFabricProject.createSimpleProject;
+
+/**
+ * @author MarcosDias
+ */
+public class ConvertToYamlControllerTest extends TestCase{
+    Project proj;
+    ConvertToYamlController conv;
+
+    public void setUp(){
+        proj = createSimpleProject();
+        conv = new ConvertToYamlController();
+    }
+
+    public void testConvertToYamlProject(){
+        String yaml = convert(proj);
+        assertNotNull(yaml);
+        assertFalse(yaml.isEmpty());
+    }
 }
