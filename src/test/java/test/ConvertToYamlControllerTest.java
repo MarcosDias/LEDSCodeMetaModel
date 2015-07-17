433f39a4046115a3a18f70c7b32f4fee5f23ec8f
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-17 17:32:21 -0300

    Atualizacoes no test

--- src/test/java/test/ConvertToYamlControllerTest.java
+++ src/test/java/test/ConvertToYamlControllerTest.java
@@ -1,22 +1,25 @@
 package test;
 
-import ctrl.ConvertToYamlController;
-import junit.framework.TestCase;
-import model.mainLayer.Project;
-
 import static ctrl.ConvertToYamlController.convert;
 import static test.util.SimpleFabricProject.createSimpleProject;
+import model.mainLayer.Project;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author MarcosDias
  */
-public class ConvertToYamlControllerTest extends TestCase{
+public class ConvertToYamlControllerTest{
     Project proj;
 
+    @Before
     public void setUp(){
         proj = createSimpleProject();
     }
 
+    @Test
     public void testConvertToYamlProject(){
         String yaml = convert(proj);
         assertNotNull(yaml);
