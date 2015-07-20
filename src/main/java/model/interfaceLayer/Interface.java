0a45d5963307cd2c124db690240bf445ab3e43d2
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-20 08:20:32 -0300

    Gets e Sets

--- src/main/java/model/interfaceLayer/Interface.java
+++ src/main/java/model/interfaceLayer/Interface.java
@@ -1,8 +1,13 @@
 package model.interfaceLayer;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author MarcosDias
  */
+@Getter
+@Setter
 public class Interface {
     private String name;
     private InterfaceApplication interfaceApplication;
