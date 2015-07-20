6547e8e135fca5a144c5af4f1a5dcb9af88871f6
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-20 08:20:16 -0300

    Método de conversao de string para enum

--- src/main/java/model/infrastructureLayer/Environment.java
+++ src/main/java/model/infrastructureLayer/Environment.java
@@ -1,8 +1,29 @@
 package model.infrastructureLayer;
 
+import lombok.Getter;
+
 /**
  * @author MarcosDias
  */
+@Getter
 public enum Environment {
-    DEV, PROD;
+    DEV("dev"), 
+    PROD("prod");
+    
+    private String valor;
+    
+    Environment(String valor){
+    	this.valor = valor;
+    }
+    
+    public static Environment fromString(String _valor) {
+        if (_valor != null) {
+          for (Environment v : Environment.values()) {
+            if (_valor.equalsIgnoreCase(v.valor)) {
+              return v;
+            }
+          }
+        }
+        return null;
+      }
 }
