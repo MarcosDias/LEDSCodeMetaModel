b75aaa0b7dab6f21ae19e57235d6e2a7c0d12cc2
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-20 18:24:29 -0300

    atributo + metodo de conversao

--- src/main/java/model/interfaceLayer/Type.java
+++ src/main/java/model/interfaceLayer/Type.java
@@ -1,8 +1,26 @@
 package model.interfaceLayer;
 
+
 /**
  * @author MarcosDias
  */
 public enum Type {
-    HtmlView, RestFull;
+    HTMLVIEW("HtmlView"), RESTFULL("RestFull");
+    
+    private String valor;
+
+	Type(String valor) {
+		this.valor = valor;
+	}
+    
+    public static Type fromString(String _valor) {
+		if (_valor != null) {
+			for (Type v : Type.values()) {
+				if (_valor.equalsIgnoreCase(v.valor)) {
+					return v;
+				}
+			}
+		}
+		return null;
+	}
 }
