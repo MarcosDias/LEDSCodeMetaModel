6b3e30ee63f7568526858bd71161ec4453953fc8
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-22 14:27:30 -0300

    Gets e Sets

--- src/main/java/model/domainLayer/Repository.java
+++ src/main/java/model/domainLayer/Repository.java
@@ -2,9 +2,14 @@ package model.domainLayer;
 
 import java.util.Set;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author MarcosDias
  */
+@Getter
+@Setter
 public class Repository {
     private String name;
     private Set<Method> methods;
