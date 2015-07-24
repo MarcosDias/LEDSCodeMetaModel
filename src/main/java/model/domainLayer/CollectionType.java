5557159ac2fd067f03507a0d06be35d174f71fc9
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-24 11:37:45 -0300

    Conversor de String para enum

--- src/main/java/model/domainLayer/CollectionType.java
+++ src/main/java/model/domainLayer/CollectionType.java
@@ -1,8 +1,28 @@
 package model.domainLayer;
 
+import lombok.Getter;
+
 /**
  * @author MarcosDias
  */
+@Getter
 public enum CollectionType {
-    Set,List;
+    SET("Set"),LIST("List");
+    
+    private String valor;
+
+    CollectionType(String valor) {
+		this.valor = valor;
+	}
+
+	public static CollectionType fromString(String _valor) {
+		if (_valor != null) {
+			for (CollectionType v : CollectionType.values()) {
+				if (_valor.equalsIgnoreCase(v.valor)) {
+					return v;
+				}
+			}
+		}
+		return null;
+	}
 }
