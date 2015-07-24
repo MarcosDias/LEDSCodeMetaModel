67d9230f7989e7590555df1dbbe36113d1e91b7e
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-24 20:40:08 -0300

    novo anotação - construtor

--- src/main/java/model/domainLayer/Entity.java
+++ src/main/java/model/domainLayer/Entity.java
@@ -1,15 +1,17 @@
 package model.domainLayer;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Set;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author MarcosDias
  */
 @Getter
 @Setter
+@NoArgsConstructor
 public class Entity extends SuperClass{
     private boolean abstrato;
     private Repository repository;
