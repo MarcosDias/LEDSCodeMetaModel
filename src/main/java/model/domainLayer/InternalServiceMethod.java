f3b28f44e6fc726e3d95b0ee213cbc1bc037d11b
Marcos Dias <marcos.a.melo.dias@gmail.com>
2015-07-26 22:03:12 -0300

    atualizacao

--- src/main/java/model/domainLayer/InternalServiceMethod.java
+++ src/main/java/model/domainLayer/InternalServiceMethod.java
@@ -11,4 +11,8 @@ import lombok.Setter;
 public class InternalServiceMethod extends ServiceMethod {
     private Method method;
     private Entity entity;
+    
+    public String methodAcess(){
+    	return this.entity.name + this.entity.getRepository().getName() + this.method.getName();
+    }
 }
